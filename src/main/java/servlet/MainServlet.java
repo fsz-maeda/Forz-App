51770f6f5279f789a39b997bf653b27d8d105961
fdc-Teraguchi <teraguchi_rin@fannel.com>
2026-06-01 14:38:55 +0900

    変更

---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Main")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/servlet/UserRegisterServlet.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import dao.UserRegisterDAO;
+
+@WebServlet("/UserRegister")
+public class UserRegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/userRegister.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		String mail = request.getParameter("mail");
+		
+		UserRegisterDAO dao = new UserRegisterDAO();
+		
+		boolean result = dao.registerUser(name, pass, mail);
+		
+		if(result) {
+			request.setAttribute("registerMsg", "登録完了");
+		}else {
+			request.setAttribute("registerMsg", "登録失敗");
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/home.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
