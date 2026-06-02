1afb7e1aa7d423d5c24fea2c65b25e09eca328cc
fsz_maeda <yuta_maeda@fsz.co.jp>
2026-06-02 10:12:39 +0900

    Port.java削除

--- src/main/java/model/Port.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class Port {
-   public static String JDBC_URL = "jdbc:sqlserver://localhost\\\\SQLEXPRESS:64433;databaseName=master;"
-			+ "integratedSecurity=true;encrypt=true;trustServerCertificate=true;";
-}
