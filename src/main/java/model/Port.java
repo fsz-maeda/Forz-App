e39b3ec9460e0e3b94292cb4731587ec21463613
fdc-rahman <rahman_habibur@fannel.com>
2026-06-03 09:15:14 +0900

    Port.javaを削除

--- src/main/java/model/Port.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Port {
-	public static String JDBC_URL ="jdbc:sqlserver://localhost\\SQLEXPRESS:60197;"
-            + "databaseName=master;"
-            + "integratedSecurity=true;"
-            + "encrypt=true;"
-            + "trustServerCertificate=true;";
-}
