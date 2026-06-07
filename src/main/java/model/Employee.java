29c9b0b0cf2ae0cb1d50e73933aa15a8316b35e5
fsz_maeda <yuta_maeda@fsz.co.jp>
2026-06-08 08:54:49 +0900

    コード修正

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -6,36 +6,45 @@ public class Employee {
 	private String department;
 	private String position;
 	private String photoPath;
-	
-	public int getEmployeeId() {return employeeId;}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
 	public void setEmployeeId(int employeeId) {
 		this.employeeId = employeeId;
 	}
-	public String getName() {return name;}
+
+	public String getName() {
+		return name;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getDepartment() {return department;}
+
+	public String getDepartment() {
+		return department;
+	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-	public String getPosition() {return position;}
+
+	public String getPosition() {
+		return position;
+	}
+
 	public void setPosition(String position) {
 		this.position = position;
 	}
-	 public String getPhotoPath() {return photoPath;}
-	 public void setPhotoPath(String photoPath) {
-		 this.photoPath = photoPath;
-	 }
-	
+
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
 
 }
-
-
-
-
-
-
-
-
-
